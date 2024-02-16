273b97176cbc002dd116fe6e5b458535524129c2
e.trier <e.trier@student.isartdigital.com>
2024-02-16 13:45:16 +0100

    Debug added

---
+++ GolemEngine/Include/Debug/log.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <filesystem>
+#include <fstream>
+
+class Log
+{
+public:
+	Log() {};
+	~Log();
+
+	static void OpenFile(std::filesystem::path const& filename);
+	static void Print(const char* format, ...);
+
+private:
+	static std::fstream mFile;
+};
---
+++ GolemEngine/Source/Debug/log.cpp
@@ -0,0 +1,40 @@
+#include "Debug/log.h"
+#include <iostream>
+#include <cstdarg>
+
+std::fstream Log::mFile;
+
+Log::~Log()
+{
+    mFile.close();
+}
+
+void Log::OpenFile(std::filesystem::path const& filename)
+{
+    mFile.open(filename, std::fstream::out);
+
+    if (mFile.is_open())
+    {
+        std::cout << "Open file: " << filename << std::endl;
+    }
+    else 
+        std::cout << "Cannot open the file: " << filename << std::endl;
+}
+
+void Log::Print(const char* format, ...)
+{
+    char buffer[1024];
+    va_list args;
+    va_start(args, format);
+        
+    vsprintf_s(buffer, sizeof(buffer), format, args);
+    std::cout << buffer << std::endl;
+
+    if (mFile.is_open())
+    {
+        mFile << buffer << std::endl;
+        mFile.flush();
+    }
+    
+    va_end(args);
+}
