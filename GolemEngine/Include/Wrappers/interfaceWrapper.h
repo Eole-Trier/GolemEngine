f332b9a9d7ddaa2ee81fa4a5099e8dc950da4a5a
ChristopheHuang <c.huang@student.isartdigital.com>
2024-03-07 18:13:27 +0100

    Added functions in InterfaceWrapper class

--- GolemEngine/Include/Wrappers/interfaceWrapper.h
+++ GolemEngine/Include/Wrappers/interfaceWrapper.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include "imgui.h"
 
 class GOLEM_ENGINE_API InterfaceWrapper
 {
@@ -11,9 +12,43 @@ public:
 
 	void Init(GLFWwindow* _window);
 	void NewFrameImGui();
+	void StyleColorsDark(ImGuiStyle* _dst = NULL);
+public:
+	ImGuiIO& GetIO();
+	ImGuiStyle& GetStyle();
+
+	ImGuiID GetID(const char* _strId);                                      
+	ImGuiID GetID(const char* _strIdBegin, const char* _strIdEnd);
+	ImGuiID GetID(const void* _ptrId);
+
+	ImGuiViewport* GetMainViewport();
+	ImDrawData* GetDrawData();
+
+	ImGuiContext* CreateContext(ImFontAtlas* _sharedFontAtlas = NULL);
 
 public:
-	void Begin(const char* _name);
+	void NewFrame();
+
+	void Begin(const char* _name, bool* _open = (bool*)0, ImGuiWindowFlags flags = 0);
+
+	void SetNextWindowPos(const ImVec2& _pos, ImGuiCond _cond = 0, const ImVec2& _pivot = ImVec2(0, 0));
+	void SetNextWindowSize(const ImVec2& _size, ImGuiCond _cond = 0);
+	void SetNextWindowViewport(ImGuiID _viewportId);
+
+	void DockSpace(ImGuiID _id, const ImVec2& _size = ImVec2(0, 0), ImGuiDockNodeFlags _flags = 0, const ImGuiWindowClass* _windowClass = NULL);
+	void DockBuilderAddNode(ImGuiID _id = 0U, ImGuiDockNodeFlags _flags = 0);
+	void DockBuilderSetNodeSize(ImGuiID _id, ImVec2 _size);
+	void DockBuilderSplitNode(ImGuiID _id, ImGuiDir _splitDir, float _sizeRatioForNodeAtDir, ImGuiID* _outIdAtDir, ImGuiID* _outIdAtOppositeDir);
+	void DockBuilderDockWindow(const char* _windowName, ImGuiID _id);
+	void DockBuilderFinish(ImGuiID _id);
+
+	void PushStyleVar(ImGuiStyleVar _idx, float _val);
+	void PushStyleVar(ImGuiStyleVar _idx, const ImVec2& _val);
+
+	void PopStyleVar(int _count = 1);
+
+	void UpdatePlatformWindows();
+	void RenderPlatformWindowsDefault(void* _platformRenderArg = NULL, void* _rendererRenderArg = NULL);
 
 	void Dock();
 	void Loop();
@@ -22,6 +57,10 @@ public:
 	void End();
 
 	void Render();
+
+public:
+	void SliderFloat(const char* _label, float* _v, float _vMin, float _vMax, const char* _format = "%.3f", ImGuiSliderFlags _flags = 0);
+
 private:
 	InterfaceWrapper();
 	~InterfaceWrapper();
--- GolemEngine/Source/Wrappers/interfaceWrapper.cpp
+++ GolemEngine/Source/Wrappers/interfaceWrapper.cpp
@@ -15,6 +15,11 @@ InterfaceWrapper* InterfaceWrapper::GetInstance()
     return m_instancePtr;
 }
 
+void InterfaceWrapper::SliderFloat(const char* _label, float* _v, float _vMin, float _vMax, const char* _format, ImGuiSliderFlags _flags)
+{
+    ImGui::SliderFloat(_label, _v, _vMin, _vMax, _format, _flags);
+}
+
 InterfaceWrapper::InterfaceWrapper() {}
 
 InterfaceWrapper::~InterfaceWrapper() {}
@@ -23,16 +28,16 @@ void InterfaceWrapper::Init(GLFWwindow* _window)
 {
     // Setup Imgui context
     IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    CreateContext();
+    ImGuiIO& io = GetIO(); (void)io;
     io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
     io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
 
     // Setup style
-    ImGui::StyleColorsDark();
+    StyleColorsDark();
 
     // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones
-    ImGuiStyle& style = ImGui::GetStyle();
+    ImGuiStyle& style = GetStyle();
     if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
     {
         style.WindowRounding = 0.0f;
@@ -47,12 +52,57 @@ void InterfaceWrapper::NewFrameImGui()
 {
     ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplGlfw_NewFrame();
-    ImGui::NewFrame();
+    NewFrame();
+}
+
+void InterfaceWrapper::StyleColorsDark(ImGuiStyle* _dst)
+{
+    ImGui::StyleColorsDark(_dst);
+}
+
+ImGuiIO& InterfaceWrapper::GetIO()
+{
+    return(ImGui::GetIO());
+}
+
+ImGuiStyle& InterfaceWrapper::GetStyle()
+{
+    return(ImGui::GetStyle());
+}
+
+ImGuiID InterfaceWrapper::GetID(const char* _strId)
+{
+    return(ImGui::GetID(_strId));
+}
+
+ImGuiID InterfaceWrapper::GetID(const char* _strIdBegin, const char* _strIdEnd)
+{
+    return(ImGui::GetID(_strIdBegin, _strIdEnd));
+}
+
+ImGuiID InterfaceWrapper::GetID(const void* _ptrId)
+{
+    return(ImGui::GetID(_ptrId));
+}
+
+ImGuiViewport* InterfaceWrapper::GetMainViewport()
+{
+    return(ImGui::GetMainViewport());
+}
+
+ImDrawData* InterfaceWrapper::GetDrawData()
+{
+    return(ImGui::GetDrawData());
+}
+
+ImGuiContext* InterfaceWrapper::CreateContext(ImFontAtlas* _sharedFontAtlas)
+{
+    return(ImGui::CreateContext(_sharedFontAtlas));
 }
 
 void InterfaceWrapper::EditorStyle()
 {
-    ImGuiStyle& style = ImGui::GetStyle();
+    ImGuiStyle& style = GetStyle();
 
     style.WindowRounding = 4;
     style.FrameRounding = 4;
@@ -71,9 +121,84 @@ void InterfaceWrapper::Render()
     ImGui::Render();
 }
 
-void InterfaceWrapper::Begin(const char* _name)
+void InterfaceWrapper::NewFrame()
 {
-    ImGui::Begin(_name);
+    ImGui::NewFrame();
+}
+
+void InterfaceWrapper::Begin(const char* _name, bool* _open, ImGuiWindowFlags flags)
+{
+    ImGui::Begin(_name, _open, flags);
+}
+
+void InterfaceWrapper::SetNextWindowPos(const ImVec2& _pos, ImGuiCond _cond, const ImVec2& _pivot)
+{
+    ImGui::SetNextWindowPos(_pos, _cond, _pivot);
+}
+
+void InterfaceWrapper::SetNextWindowSize(const ImVec2& _size, ImGuiCond _cond)
+{
+    ImGui::SetNextWindowSize(_size, _cond);
+}
+
+void InterfaceWrapper::SetNextWindowViewport(ImGuiID _viewportId)
+{
+    ImGui::SetNextWindowViewport(_viewportId);
+}
+
+void InterfaceWrapper::DockSpace(ImGuiID _id, const ImVec2& _size, ImGuiDockNodeFlags _flags, const ImGuiWindowClass* _windowClass)
+{
+    ImGui::DockSpace(_id, _size, _flags, _windowClass);
+}
+
+void InterfaceWrapper::DockBuilderAddNode(ImGuiID _id, ImGuiDockNodeFlags _flags)
+{
+    ImGui::DockBuilderAddNode(_id, _flags);
+}
+
+void InterfaceWrapper::DockBuilderSetNodeSize(ImGuiID _id, ImVec2 _size)
+{
+    ImGui::DockBuilderSetNodeSize(_id, _size);
+}
+
+void InterfaceWrapper::DockBuilderSplitNode(ImGuiID _id, ImGuiDir _splitDir, float _sizeRatioForNodeAtDir, ImGuiID* _outIdAtDir, ImGuiID* _outIdAtOppositeDir)
+{
+    ImGui::DockBuilderSplitNode(_id, _splitDir, _sizeRatioForNodeAtDir, _outIdAtDir, _outIdAtOppositeDir);
+}
+
+void InterfaceWrapper::DockBuilderDockWindow(const char* _windowName, ImGuiID _id)
+{
+    ImGui::DockBuilderDockWindow(_windowName, _id);
+}
+
+void InterfaceWrapper::DockBuilderFinish(ImGuiID _id)
+{
+    ImGui::DockBuilderFinish(_id);
+}
+
+void InterfaceWrapper::PushStyleVar(ImGuiStyleVar _idx, float _val)
+{
+    ImGui::PushStyleVar(_idx, _val);
+}
+
+void InterfaceWrapper::PushStyleVar(ImGuiStyleVar _idx, const ImVec2& _val)
+{
+    ImGui::PushStyleVar(_idx, _val);
+}
+
+void InterfaceWrapper::PopStyleVar(int _count)
+{
+    ImGui::PopStyleVar(_count);
+}
+
+void InterfaceWrapper::UpdatePlatformWindows()
+{
+    ImGui::UpdatePlatformWindows();
+}
+
+void InterfaceWrapper::RenderPlatformWindowsDefault(void* _platformRenderArg, void* _rendererRenderArg)
+{
+    ImGui::RenderPlatformWindowsDefault(_platformRenderArg, _rendererRenderArg);
 }
 
 void InterfaceWrapper::Dock()
@@ -91,75 +216,75 @@ void InterfaceWrapper::Dock()
     ImGuiWindowFlags windowFlags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
     if (optFullscreen)
     {
-        const ImGuiViewport* viewport = ImGui::GetMainViewport();
-        ImGui::SetNextWindowPos(viewport->WorkPos);
-        ImGui::SetNextWindowSize(viewport->WorkSize);
-        ImGui::SetNextWindowViewport(viewport->ID);
-        ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
-        ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
-        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, { 0, 0 });
+        const ImGuiViewport* viewport = GetMainViewport();
+        SetNextWindowPos(viewport->WorkPos);
+        SetNextWindowSize(viewport->WorkSize);
+        SetNextWindowViewport(viewport->ID);
+        PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+        PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+        PushStyleVar(ImGuiStyleVar_WindowPadding, { 0, 0 });
 
         windowFlags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
         windowFlags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus | ImGuiWindowFlags_NoBackground;
     }
 
-    ImGui::Begin("DockSpace Demo", &dockspaceOpen, windowFlags);
+    Begin("DockSpace Demo", &dockspaceOpen, windowFlags);
 
-    ImGuiID dockspace_id = ImGui::GetID("DockSpace");
-    ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspaceFlags);
+    ImGuiID dockspace_id = GetID("DockSpace");
+    DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspaceFlags);
 
     static bool init = true;
     if (init)
     {
         ImGuiID dock_id_left, dock_id_right;
         init = false;
-        ImGui::DockBuilderAddNode(dockspace_id);
-        ImGui::DockBuilderSetNodeSize(dockspace_id, ImGui::GetMainViewport()->Size);
+        DockBuilderAddNode(dockspace_id);
+        DockBuilderSetNodeSize(dockspace_id, ImGui::GetMainViewport()->Size);
 
-        ImGui::DockBuilderSplitNode(dockspace_id, ImGuiDir_Left, 0.8f, &dock_id_left, &dock_id_right);
+        DockBuilderSplitNode(dockspace_id, ImGuiDir_Left, 0.8f, &dock_id_left, &dock_id_right);
 
         ImGuiID dock_id_topRight, dock_id_bottomRight;
-        ImGui::DockBuilderSplitNode(dock_id_right, ImGuiDir_Up, 0.8f, &dock_id_topRight, &dock_id_bottomRight);
+        DockBuilderSplitNode(dock_id_right, ImGuiDir_Up, 0.8f, &dock_id_topRight, &dock_id_bottomRight);
 
         ImGuiID dock_id_topLeft, dock_id_bottomLeft;
-        ImGui::DockBuilderSplitNode(dock_id_left, ImGuiDir_Up, 0.8f, &dock_id_topLeft, &dock_id_bottomLeft);
+        DockBuilderSplitNode(dock_id_left, ImGuiDir_Up, 0.8f, &dock_id_topLeft, &dock_id_bottomLeft);
 
-        ImGui::DockBuilderDockWindow("Basic_Actors", dock_id_topRight);
-        ImGui::DockBuilderDockWindow("File_Browser", dock_id_bottomLeft);
-        ImGui::DockBuilderDockWindow("Viewport", dock_id_topLeft);
-        ImGui::DockBuilderDockWindow("World_Actors", dock_id_topRight);
-        ImGui::DockBuilderDockWindow("Debug", dock_id_bottomRight);
+        DockBuilderDockWindow("Basic_Actors", dock_id_topRight);
+        DockBuilderDockWindow("File_Browser", dock_id_bottomLeft);
+        DockBuilderDockWindow("Viewport", dock_id_topLeft);
+        DockBuilderDockWindow("World_Actors", dock_id_topRight);
+        DockBuilderDockWindow("Debug", dock_id_bottomRight);
 
-        ImGui::DockBuilderFinish(dockspace_id);
+        DockBuilderFinish(dockspace_id);
     }
 
     if (optFullscreen)
     {
-        ImGui::PopStyleVar(3);
+        PopStyleVar(3);
     }
 }
 
 void InterfaceWrapper::Loop()
 {
-    ImGuiIO& io = ImGui::GetIO();
+    ImGuiIO& io = GetIO();
     Camera* camera = Camera::instance;
 
     Begin("Camera");
 
-    ImGui::SliderFloat("Camera Move Speed : ",  &camera->movementSpeed, 0, 30.0f, 0);
-    ImGui::SliderFloat("Camera Move Sensitivity : ", &camera->mouseSensitivity, 0, 2.0f, 0);
+    SliderFloat("Camera Move Speed : ",  &camera->movementSpeed, 0, 30.0f, 0);
+    SliderFloat("Camera Move Sensitivity : ", &camera->mouseSensitivity, 0, 2.0f, 0);
 
     End();
 
     Render();
 
-    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    ImGui_ImplOpenGL3_RenderDrawData(GetDrawData());
 
     if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
     {
         GLFWwindow* backup_current_context = glfwGetCurrentContext();
-        ImGui::UpdatePlatformWindows();
-        ImGui::RenderPlatformWindowsDefault();
+        UpdatePlatformWindows();
+        RenderPlatformWindowsDefault();
         glfwMakeContextCurrent(backup_current_context);
     }
 }
