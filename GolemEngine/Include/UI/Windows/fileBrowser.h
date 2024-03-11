8562da81e92db1a69b371ea00750e4bf3621d953
Clément TAILLEE <c.taillee@student.isartdigital.com>
2024-03-12 00:30:03 +0100

    start doing file browser

--- GolemEngine/Include/UI/Windows/fileBrowser.h
+++ GolemEngine/Include/UI/Windows/fileBrowser.h
@@ -1,11 +1,18 @@
 #pragma once
 #include "UI/engineUi.h"
 
+#include <filesystem>
+#include <string>
+
 class FileBrowser : Window
 {
 public:
 	FileBrowser();
 	~FileBrowser();
+
+	void Init();
+
+	void TreeNodes(std::filesystem::path path);
 
 	void Update();
 };
--- GolemEngine/Source/UI/Windows/fileBrowser.cpp
+++ GolemEngine/Source/UI/Windows/fileBrowser.cpp
@@ -3,7 +3,9 @@
 #include "imgui.h"
 #include "ImGuiFileDialog-master/ImGuiFileDialog.h"
 #include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
+#include "imgui_impl_opengl3.h"*
+
+
 
 FileBrowser::FileBrowser() {}
 
@@ -11,24 +13,46 @@ FileBrowser::~FileBrowser() {}
 
 void FileBrowser::Update()
 {
+	Init();
+	//ImGui::ShowDemoWindow();
+}
+
+void FileBrowser::Init()
+{
 	ImGui::Begin("File_Browser");
 
-    if (ImGui::Button("Open File Dialog"))
-    {
-        ImGuiFileDialog::Instance()->OpenDialog("ChooseFileDlgKey", "Choose File", ".cpp,.h,.hpp,.py", ".");
-    }
+	ImGui::BeginChild("child1", ImVec2(ImGui::GetContentRegionAvail().x * 0.5f, 0), ImGuiChildFlags_Border | ImGuiChildFlags_ResizeX);
+	TreeNodes(std::filesystem::current_path());
+	ImGui::EndChild();
 
-    if (ImGuiFileDialog::Instance()->Display("ChooseFileDlgKey"))
-    {
-        if (ImGuiFileDialog::Instance()->IsOk())
-        {
-            std::string filePathName = ImGuiFileDialog::Instance()->GetFilePathName();
-            std::string filePath = ImGuiFileDialog::Instance()->GetCurrentPath();
-            system(filePathName.c_str());
-        }
-
-        ImGuiFileDialog::Instance()->Close();
-    }
+	ImGui::SameLine();
+	ImGui::BeginChild("child2", ImVec2(0, 0), ImGuiChildFlags_Border);
+	ImGui::EndChild();
 
 	ImGui::End();
 }
+
+void FileBrowser::TreeNodes(std::filesystem::path path)
+{
+	bool test = std::filesystem::is_directory(path);
+
+	if (test)
+	{
+		if (ImGui::TreeNodeEx(path.filename().string().c_str(), ImGuiTreeNodeFlags_OpenOnArrow))
+		{
+			for (const auto& entry : std::filesystem::directory_iterator(path))
+			{
+				TreeNodes(entry);
+			}
+
+			ImGui::TreePop();
+		}
+	}
+
+	else
+	{
+		ImGui::TreeNodeEx(path.filename().string().c_str(), ImGuiTreeNodeFlags_Leaf);
+		ImGui::TreePop();
+	}
+}
+
