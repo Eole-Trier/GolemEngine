35fd25fc1001ba57f1f773ec980b5117c96f6950
christophe <c.huang@student.isartdigital.com>
2024-03-12 11:03:46 +0100

    Added ContentBrowser function in fileBrowser, can see all the file in editor

--- GolemEditor/Include/UI/Windows/fileBrowser.h
+++ GolemEditor/Include/UI/Windows/fileBrowser.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <filesystem>
+#include <string>
+
 class GolemEngine;
 
 class FileBrowser
@@ -9,4 +12,16 @@ public:
 	~FileBrowser();
 
 	void Update(GolemEngine* _golemEngine, const char* _name = "default_window");
+
+	void TreeNodes(std::filesystem::path _path);
+
+	void ContentBrowser();
+
+	const char* GetFileName(const char* _path);
+
+private:
+
+	const std::string m_editorDirectory = std::filesystem::current_path().string().c_str();
+
+	std::filesystem::path m_currentDirectory;
 };
--- GolemEditor/Source/UI/Windows/fileBrowser.cpp
+++ GolemEditor/Source/UI/Windows/fileBrowser.cpp
@@ -1,5 +1,7 @@
 #include "UI/Windows/fileBrowser.h"
 
+#include <filesystem>
+
 #include "ImGuiFileDialog-master/ImGuiFileDialog.h"
 #include "golemEngine.h"
 #include "imgui.h"
@@ -7,15 +9,80 @@
 #include "imgui_impl_opengl3.h"
 #include "imgui_internal.h"
 
+namespace fs = std::filesystem;
 
-FileBrowser::FileBrowser() {}
+FileBrowser::FileBrowser() 
+	:m_currentDirectory(m_editorDirectory)
+{}
 
 FileBrowser::~FileBrowser() {}
 
 void FileBrowser::Update(GolemEngine* _golemEngine, const char* _name)
 {
-    ImGui::Begin("File_Browser");
+	ImGui::Begin("File_Browser");
+
+	ImGui::BeginChild("child1", ImVec2(ImGui::GetContentRegionAvail().x * 0.5f, 0), ImGuiChildFlags_Border | ImGuiChildFlags_ResizeX);
+	TreeNodes(std::filesystem::current_path());
+	ImGui::EndChild();
+
+	ImGui::SameLine();
+	ImGui::BeginChild("child2", ImVec2(0, 0), ImGuiChildFlags_Border);
+	ContentBrowser();
+	ImGui::EndChild();
+
+	ImGui::End();
+}
 
 
-    ImGui::End();
+void FileBrowser::TreeNodes(std::filesystem::path _path)
+{
+	bool test = std::filesystem::is_directory(_path);
+	if (test)
+	{
+		if (ImGui::TreeNodeEx(_path.filename().string().c_str(), ImGuiTreeNodeFlags_OpenOnArrow))
+		{
+			for (const auto& entry : std::filesystem::directory_iterator(_path))
+			{
+				TreeNodes(entry);
+			}
+			ImGui::TreePop();
+		}
+	}
+	else
+	{
+		ImGui::TreeNodeEx(_path.filename().string().c_str(), ImGuiTreeNodeFlags_Leaf);
+		ImGui::TreePop();
+	}
+}
+
+void FileBrowser::ContentBrowser()
+{
+	for (auto& p : fs::directory_iterator(m_currentDirectory))
+	{
+		std::string path = p.path().string();
+		if (p.is_directory())
+		{
+			ImGui::SameLine();
+			if (ImGui::Button(GetFileName(path.c_str())))
+			{
+				m_currentDirectory = path;
+			}
+		}
+	}
+}
+
+const char* FileBrowser::GetFileName(const char* _path)
+{
+	if (_path == nullptr)
+		return nullptr;
+
+	int length = static_cast<int>(strlen(_path));
+	int index = length - 1;
+
+	while (index >= 0 && _path[index] != '\\')
+	{
+		--index;
+	}
+
+	return _path + index + 1;
 }
