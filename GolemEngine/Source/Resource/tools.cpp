4d1a0ebda7168ece2afe533c0fee7898c65708c5
christophe <c.huang@student.isartdigital.com>
2024-04-05 15:38:55 +0200

    Added a new function findfolder

--- GolemEngine/Source/Resource/tools.cpp
+++ GolemEngine/Source/Resource/tools.cpp
@@ -21,4 +21,18 @@ namespace Tools
         std::cout << "Not found the file: " << _file << std::endl;
         return ""; // Return an empty string
     }
+    std::string GOLEM_ENGINE_API FindFolder(const std::string& _folderName)
+    {
+        for (const auto& dir : std::filesystem::recursive_directory_iterator(std::filesystem::current_path()))
+        {
+            if (dir.is_directory() && dir.path().filename().string().find(_folderName) != std::string::npos)
+            {
+                return dir.path().string();
+            }
+        }
+
+        // If the folder is not found, output a message
+        std::cout << "Not found the folder: " << _folderName << std::endl;
+        return ""; // Return an empty string
+    }
 }
