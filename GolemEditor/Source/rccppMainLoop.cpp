8eac2410b9b1f5f3b7b51dd096db626bd02da716
ChristopheHuang <c.huang@student.isartdigital.com>
2024-04-16 00:58:02 +0200

    RCCPP library testing

---
+++ GolemEditor/Source/rccppMainLoop.cpp
@@ -0,0 +1,35 @@
+#include "ObjectInterfacePerModule.h"
+#include "IObject.h"
+#include "rccppMainLoop.h"
+#include "systemTable.h"
+
+#include <iostream>
+
+// RCC++ uses interface Id's to distinguish between different classes
+// here we have only one, so we don't need a header for this enum and put it in the same
+// source code file as the rest of the code
+enum InterfaceIDEnumConsoleExample
+{
+    IID_IRCCPP_MAIN_LOOP = IID_ENDInterfaceID, // IID_ENDInterfaceID from IObject.h InterfaceIDEnum
+
+    IID_ENDInterfaceIDEnumConsoleExample
+};
+
+struct RCCppMainLoop : RCCppMainLoopI, TInterface<IID_IRCCPP_MAIN_LOOP, IObject>
+{
+    RCCppMainLoop()
+    {
+        PerModuleInterface::g_pSystemTable->pRCCppMainLoopI = this;
+    }
+    
+    void MainLoop() override
+    {
+    }
+};
+
+//struct RCCppMainLoop : TInterface<IID_IRCCPP_MAIN_LOOP, IObject>
+//{
+//
+//};
+
+REGISTERSINGLETON(RCCppMainLoop, true);
