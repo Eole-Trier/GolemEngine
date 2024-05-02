b47ff35de9e35ac5e4462c6f7bb7ab1afb7f9ddc
Maxime Leguevacques <m.leguevacques@student.isartdigital.com>
2024-05-02 15:53:49 +0200

    changed order of draw which looks more logical

--- GolemEngine/Source/Core/scene.cpp
+++ GolemEngine/Source/Core/scene.cpp
@@ -88,8 +88,8 @@ void Scene::UpdateTerrains(Camera* _camera)
     for (int i = 0; i < m_terrains.size(); i++)
     {
         m_terrains[i]->UseComputeShader();
-        m_terrains[i]->GetComputeShaderData(_camera);
         m_terrains[i]->Draw(_camera);
+        m_terrains[i]->GetComputeShaderData(_camera);
     }
 }
 
