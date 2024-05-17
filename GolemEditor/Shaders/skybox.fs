889ec4b5c888d9a434f4a33d9af6dd888d15229e
c.taillee <c.taillee@student.isartdigital.com>
2024-05-17 15:23:19 +0200

    fix one issue

--- GolemEditor/Shaders/skybox.fs
+++ GolemEditor/Shaders/skybox.fs
@@ -1,11 +1,13 @@
 #version 330 core
 out vec4 FragColor;
+out int FragColor2;
 
 in vec3 TexCoords;
 
 uniform samplerCube skybox;
 
 void main()
-{    
+{   
     FragColor = texture(skybox, TexCoords);
+    FragColor2 = 0;
 }
