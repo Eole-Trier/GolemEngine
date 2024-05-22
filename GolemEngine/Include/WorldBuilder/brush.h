e779f533e926ff2fbd069f074d6f5e075793da00
Maxime Leguevacques <m.leguevacques@student.isartdigital.com>
2024-05-22 20:20:32 +0200

    not doing raycast

--- GolemEngine/Include/WorldBuilder/brush.h
+++ GolemEngine/Include/WorldBuilder/brush.h
@@ -1,25 +1,17 @@
 ﻿#pragma once
 
-#include <vector3.h>
 
 #include "dll.h"
-#include "Core/camera.h"
 
 
 class GOLEM_ENGINE_API Brush
 {
-private:
-    Camera* m_camera = nullptr;
 public:
     float radius;
     float force;
 
 public:
-    Brush(Camera* _camera);
+    Brush();
 
     void Update();
-    Vector3 CalculateMouseRay(int _width, int _height, int _mouseX, int _mouseY, Camera* _camera);
-    Vector2 GetNormalisedDeviceCoordinates(int _width, int _height, float _mouseX, float _mouseY);
-    Vector4 ToEyeCoords(Vector4 _clipCoords, Camera* _camera);
-    
 };
